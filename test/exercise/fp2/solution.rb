ca5e9abc7f4f4a598a17d3bcdc76ab36630889cf
Igor Iamshchikov <iamshchikov.is@gmail.com>
2022-10-19 22:28:36 +0600

    add recursion

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,33 +5,32 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
-        i = 0
-        while i < size
-          yield self[i]
-          i += 1
-        end
-        self
+      def my_each(&block)
+        first, *rest = self
+        yield first
+        rest.empty? ? [first] : [first, *MyArray.new(rest).my_each(&block)]
       end
 
       # Написать свою функцию my_map
       def my_map
-        my_array = MyArray.new
-        my_each { |el| my_array << yield(el) }
-        my_array
+        MyArray.new(my_reduce([]) { |acc, el| acc << yield(el) })
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        my_array = MyArray.new
-        my_each { |el| my_array << el unless el.nil? }
-        my_array
+        MyArray.new(my_reduce([]) { |acc, el| el.nil? ? acc : acc << el })
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(acc = nil)
-        my_each { |el| acc = acc ? yield(acc, el) : el }
-        acc
+      def my_reduce(acc = nil, &block)
+        first, *rest = self
+        if acc.nil?
+          acc = yield(first, rest[0])
+          return MyArray.new(rest[1..(rest.size - 1)]).my_reduce(acc, &block)
+        else
+          acc = yield(acc, first)
+        end
+        rest.empty? ? acc : MyArray.new(rest).my_reduce(acc, &block)
       end
     end
   end
